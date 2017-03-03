2d3a2b7a7f654a41348e285467c0d2bf4fcaabd6
Chen Minqiang <ptpt52@gmail.com>
2017-03-03 16:18:09 +0800

    luci: ptpt52 simple custom

--- modules/luci-mod-status/luasrc/controller/admin/status.lua
+++ modules/luci-mod-status/luasrc/controller/admin/status.lua
@@ -15,8 +15,10 @@ function index()
 
 	entry({"admin", "status", "routes"}, template("admin_status/routes"), _("Routes"), 3)
 	entry({"admin", "status", "syslog"}, call("action_syslog"), _("System Log"), 4)
+	if not nixio.fs.access("/etc/adv_luci_disabled") then
 	entry({"admin", "status", "dmesg"}, call("action_dmesg"), _("Kernel Log"), 5)
 	entry({"admin", "status", "processes"}, form("admin_status/processes"), _("Processes"), 6)
+	end
 
 	entry({"admin", "status", "realtime"}, alias("admin", "status", "realtime", "load"), _("Realtime Graphs"), 7)
 
--- modules/luci-mod-system/luasrc/controller/admin/system.lua
+++ modules/luci-mod-system/luasrc/controller/admin/system.lua
@@ -13,10 +13,14 @@ function index()
 	entry({"admin", "system", "admin", "dropbear"}, view("system/dropbear"), _("SSH Access"), 2)
 		.uci_depends = { dropbear = true }
 
+	if not fs.access("/etc/adv_luci_disabled") then
 	entry({"admin", "system", "admin", "sshkeys"}, view("system/sshkeys"), _("SSH-Keys"), 3)
 		.uci_depends = { dropbear = true }
+	end
 
+	if not fs.access("/etc/adv_luci_disabled") then
 	entry({"admin", "system", "startup"}, view("system/startup"), _("Startup"), 45)
+	end
 	entry({"admin", "system", "crontab"}, view("system/crontab"), _("Scheduled Tasks"), 46)
 
 	entry({"admin", "system", "mounts"}, view("system/mounts"), _("Mount Points"), 50)
