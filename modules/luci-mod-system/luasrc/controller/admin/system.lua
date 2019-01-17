18e8ac36ce3853ee9bced48d76dd7ddea90bbdf3
Chen Minqiang <ptpt52@gmail.com>
2019-01-17 21:15:09 +0800

    luci: default ipaddr 192.168.15.1

--- modules/luci-mod-system/luasrc/controller/admin/system.lua
+++ modules/luci-mod-system/luasrc/controller/admin/system.lua
@@ -188,7 +188,7 @@ function action_sysupgrade()
 		luci.template.render("admin_system/applyreboot", {
 			title = luci.i18n.translate("Flashing..."),
 			msg   = luci.i18n.translate("The system is flashing now.<br /> DO NOT POWER OFF THE DEVICE!<br /> Wait a few minutes before you try to reconnect. It might be necessary to renew the address of your computer to reach the device again, depending on your settings."),
-			addr  = (#keep > 0) and (#force > 0) and "192.168.1.1" or nil
+			addr  = (#keep > 0) and (#force > 0) and "192.168.15.1" or nil
 		})
 		luci.sys.process.exec({ "/bin/sh", "-c","sleep 1; killall dropbear uhttpd; sleep 1; /sbin/sysupgrade %s %s %q" %{ keep, force, image_tmp } }, nil, nil, true)
 	end
@@ -267,7 +267,7 @@ function action_reset()
 		luci.template.render("admin_system/applyreboot", {
 			title = luci.i18n.translate("Erasing..."),
 			msg   = luci.i18n.translate("The system is erasing the configuration partition now and will reboot itself when finished."),
-			addr  = "192.168.1.1"
+			addr  = "192.168.15.1"
 		})
 
 		if nixio.fs.access("/rom/lib/preinit/79_disk_ready") then
