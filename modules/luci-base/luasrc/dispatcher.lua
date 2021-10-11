268065a5932bd356f26b0739b25984d2128ddd8a
Jo-Philipp Wich <jo@mein.io>
2021-10-11 19:01:39 +0200

    luci-base: dispatcher: fix null access on dispatching unknown urls
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- modules/luci-base/luasrc/dispatcher.lua
+++ modules/luci-base/luasrc/dispatcher.lua
@@ -931,7 +931,9 @@ function dispatch(request)
 			return
 		end
 
-		page.readonly = not perm
+		if page then
+			page.readonly = not perm
+		end
 	end
 
 	local action = (page and type(page.action) == "table") and page.action or {}
