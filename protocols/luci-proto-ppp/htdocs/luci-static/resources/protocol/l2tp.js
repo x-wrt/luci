3e93b11b400be8734497fbdc737ec199e9ea0e8e
Chen Minqiang <ptpt52@gmail.com>
2023-03-26 19:37:53 +0800

    protocols: Add keepalive option to L2TP protocol configuration

--- protocols/luci-proto-ppp/htdocs/luci-static/resources/protocol/l2tp.js
+++ protocols/luci-proto-ppp/htdocs/luci-static/resources/protocol/l2tp.js
@@ -5,6 +5,24 @@
 
 network.registerPatternVirtual(/^l2tp-.+$/);
 
+function write_keepalive(section_id, value) {
+	var f_opt = this.map.lookupOption('_keepalive_failure', section_id),
+	    i_opt = this.map.lookupOption('_keepalive_interval', section_id),
+	    f = (f_opt != null) ? +f_opt[0].formvalue(section_id) : null,
+	    i = (i_opt != null) ? +i_opt[0].formvalue(section_id) : null;
+
+	if (f == null || f == '' || isNaN(f))
+		f = 0;
+
+	if (i == null || i == '' || isNaN(i) || i < 1)
+		i = 1;
+
+	if (f > 0)
+		uci.set('network', section_id, 'keepalive', '%d %d'.format(f, i));
+	else
+		uci.unset('network', section_id, 'keepalive');
+}
+
 return network.registerProtocol('l2tp', {
 	getI18n: function() {
 		return _('L2TP');
@@ -53,6 +71,34 @@ return network.registerProtocol('l2tp', {
 			o.value('1', _('Manual'));
 			o.default = 'auto';
 		}
+
+		o = s.taboption('advanced', form.Value, 'pppd_options', _('Extra pppd options'), _('e.g: dump'));
+
+		o = s.taboption('advanced', form.Value, '_keepalive_failure', _('LCP echo failure threshold'), _('Presume peer to be dead after given amount of LCP echo failures, use 0 to ignore failures'));
+		o.placeholder = '0';
+		o.datatype    = 'uinteger';
+		o.write       = write_keepalive;
+		o.remove      = write_keepalive;
+		o.cfgvalue = function(section_id) {
+			var v = uci.get('network', section_id, 'keepalive');
+			if (typeof(v) == 'string' && v != '') {
+				var m = v.match(/^(\d+)[ ,]\d+$/);
+				return m ? m[1] : v;
+			}
+		};
+
+		o = s.taboption('advanced', form.Value, '_keepalive_interval', _('LCP echo interval'), _('Send LCP echo requests at the given interval in seconds, only effective in conjunction with failure threshold'));
+		o.placeholder = '5';
+		o.datatype    = 'min(1)';
+		o.write       = write_keepalive;
+		o.remove      = write_keepalive;
+		o.cfgvalue = function(section_id) {
+			var v = uci.get('network', section_id, 'keepalive');
+			if (typeof(v) == 'string' && v != '') {
+				var m = v.match(/^\d+[ ,](\d+)$/);
+				return m ? m[1] : v;
+			}
+		};
 
 		o = s.taboption('advanced', form.Value, 'mtu', _('Override MTU'));
 		o.placeholder = dev ? (dev.getMTU() || '1500') : '1500';
