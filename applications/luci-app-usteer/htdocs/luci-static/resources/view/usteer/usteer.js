6c81bc6a51b162bcb62a9bf8527fa2ca782cafe0
Chen Minqiang <ptpt52@gmail.com>
2023-12-18 09:06:43 +0800

    luci-app-usteer: refine and move to MESH menu

--- applications/luci-app-usteer/htdocs/luci-static/resources/view/usteer/usteer.js
+++ applications/luci-app-usteer/htdocs/luci-static/resources/view/usteer/usteer.js
@@ -353,7 +353,7 @@ return view.extend({
 		collectWlanAPInfos(compactconnectioninfo_table_entries, Localinfo);
 		collectWlanAPInfos(compactconnectioninfo_table_entries, Remoteinfo);
 		cbi_update_table(nodes.querySelector('#compactconnectioninfo_table'), compactconnectioninfo_table_entries, E('em', _('No data')));
-		
+
 		for (var mac in Clients) {
 			var macn = mac.toUpperCase().replace(/:/g,'');
 			var client_table_entries = [];
