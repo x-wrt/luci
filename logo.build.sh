47331a776104bebd12e47b1ca40d724a222de0b6
Chen Minqiang <ptpt52@gmail.com>
2024-07-23 13:51:30 +0800

    themes: update logo with logo.build.sh

---
+++ logo.build.sh
@@ -0,0 +1,7 @@
+
+inkscape -z -w 180 themes/luci-theme-bootstrap/htdocs/luci-static/bootstrap/logo.svg -e themes/luci-theme-openwrt-2020/htdocs/luci-static/openwrt2020/logo.png
+inkscape -z -w 48 themes/luci-theme-bootstrap/htdocs/luci-static/bootstrap/logo.svg -e themes/luci-theme-bootstrap/htdocs/luci-static/bootstrap/logo_48.png
+cp themes/luci-theme-bootstrap/htdocs/luci-static/bootstrap/logo_48.png themes/luci-theme-material/htdocs/luci-static/material/logo_48.png
+
+cp themes/luci-theme-bootstrap/htdocs/luci-static/bootstrap/logo.svg themes/luci-theme-openwrt-2020/htdocs/luci-static/openwrt2020/logo.svg
+cp themes/luci-theme-bootstrap/htdocs/luci-static/bootstrap/logo.svg themes/luci-theme-material/htdocs/luci-static/material/logo.svg
